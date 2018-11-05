35f7fde2471c8668471b657181d5c4b7ff4b682b
alexxhd <ahughes-davies1@sheffield.ac.uk>
2018-11-05 17:00:57 +0000

    created tables for degrees and departments

--- SQL Files/build_database.sql
+++ SQL Files/build_database.sql
@@ -10,8 +10,39 @@ CREATE TABLE student (
     title CHAR (2),
     forename VARCHAR (50),
     surname VARCHAR (50),
+    personal_tutor VARCHAR (50), 
     email VARCHAR (50),
     PRIMARY KEY (login),
     FOREIGN KEY (login) REFERENCES user (login)
+);
+
+CREATE TABLE department (
+    code CHAR (3),
+    name VARCHAR (100),
+    PRIMARY KEY (code)
+);
+
+CREATE TABLE degree (
+    code CHAR (6),
+    name VARCHAR (100),
+    PRIMARY KEY (code)
+);
+
+CREATE TABLE degree_level (
+    code CHAR (6),
+    level INT (1),
+    PRIMARY KEY (code, level),
+    FOREIGN KEY (code) REFERENCES degree (code)
+);
+
+-- linker table to store degree associations with departments, 
+-- and whether a department is the lead department
+CREATE TABLE associated_departments (
+    degree_code CHAR (6),
+    department_code CHAR (3),
+    lead BOOL,
+    PRIMARY KEY (degree_code, department_code),
+    FOREIGN KEY (degree_code) REFERENCES degree (code),
+    FOREIGN KEY (department_code) REFERENCES department (code)
 );
 
--- SQL Files/insert_test_items.sql
+++ SQL Files/insert_test_items.sql
@@ -1,19 +1,48 @@
--- fills the database with example objects
 
+--- INSERTING EXAMPLE USERS ---
 
 INSERT INTO user VALUES 
     ("JSmith24", "0123456789qwertyuiop0123456789", 1);
 
 INSERT INTO student VALUES 
-    ("JSmith24", "Mr", "John", "Smith", "jsmith24@shef.ac.uk");
+    ("JSmith24", "Mr", "John", "Smith", "Professor Oak", "jsmith24@shef.ac.uk");
 
 INSERT INTO user VALUES
-    ("PGreen", "qwertyuiopasdfghjkl;1234567890", 4);
-
-INSERT INTO user VALUES
-    ("HWhite", "123456789012345678901234567890", 3);
-
-INSERT INTO user VALUES
+    ("PGreen", "qwertyuiopasdfghjkl;1234567890", 4),
+    ("HWhite", "123456789012345678901234567890", 3),
     ("MBlack", "123456789012345678901234567890", 2);
+
+
+
+--- INSERTING EXAMPLE DEGREES AND DEPARTMENTS ---
+
+INSERT INTO degree VALUES 
+    ("COMU03", "MEng Software Engineering");
+
+INSERT INTO degree_level VALUES 
+    ("COMU03", 1),
+    ("COMU03", 2),
+    ("COMU03", 3),
+    ("COMU03", 4);
+
+INSERT INTO degree VALUES 
+    ("COMU49", "BSc Philosophy and Computer Science");
+
+INSERT INTO degree_level VALUES
+    ("COMU49", 1),
+    ("COMU49", 2),
+    ("COMu49", 3);
+
+INSERT INTO department VALUES 
+    ("COM", "Computer Science"),
+    ("PHI", "Philosophy");
+
+INSERT INTO associated_departments VALUES
+    ("COMU49", "COM", 1),
+    ("COMU49", "PHI", 0),
+    ("COMU03", "COM", 1)
+
+ 
+
 
 
