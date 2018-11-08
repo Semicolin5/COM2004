bd39220dfc8073975715067560271de8bb4d5938
danny <dannycg1996@gmail.com>
2018-11-08 19:07:55 +0000

    Basic Module Form and button.

---
+++ source/Module.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Module {
+    private JButton createModuleButton;
+    private JPanel myPanel;
+    private JTextField moduleCode;
+    private JTextField module_Name;
+    private JTextField moduleCredits;
+
+    public Module() {
+        createModuleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Module Created" );
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Module");
+        frame.setContentPane(new Module().myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(550, 200);
+        frame.setVisible(true);
+    }
+}
