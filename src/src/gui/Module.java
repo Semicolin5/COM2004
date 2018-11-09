e18c4842ecf79db001e2958918d38f5da0d083b8
danny <dannycg1996@gmail.com>
2018-11-09 00:26:31 +0000

    Moved into the gui folder and made minor tweaks to forms.

--- src/src/gui/Module.java
+++ src/src/gui/Module.java
@@ -1,6 +1,8 @@
-import javax.swing.*;
+package gui;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class Module {
     private JButton createModuleButton;
