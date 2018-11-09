7df00edfd81dba6f70d3eb1d85f7e0c5363216aa
danny <dannycg1996@gmail.com>
2018-11-09 00:40:43 +0000

    Added appropriate comments and tweaked the GUI to be more suitable (minor changes).

--- src/src/gui/Degree.java
+++ src/src/gui/Degree.java
@@ -6,20 +6,22 @@ import java.awt.event.ActionListener;
 
 public class Degree {
     private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton saveButton;
+    private JTextField degreeCode;
+    private JTextField degreeName;
+    private JButton createDegree;
 
     public Degree() {
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                    JOptionPane.showMessageDialog(null, "Degree Created" );
-                }
-        });
+        createDegree.addActionListener(e -> {
+            //Add function to create degree here.
+                JOptionPane.showMessageDialog(null, "Degree Created" );
+            });
     }
 
     public static void main(String[] args) {
+        /**
+         * Creates a GUI in Java Swing, which can be used to easily enter data
+         * which will then be saved to the SQL database.
+         * */
         JFrame frame = new JFrame("Degree");
         frame.setContentPane(new Degree().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
