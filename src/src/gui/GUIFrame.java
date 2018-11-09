9b22c814f1021f2a8b3706a6c21f587021dbbd59
Edward Higgins <ewnhiggins1@sheffield.ac.uk>
2018-11-09 11:36:51 +0000

    Abstracted out JPanel transitions

--- src/src/gui/GUIFrame.java
+++ src/src/gui/GUIFrame.java
@@ -4,10 +4,22 @@ import javax.swing.*;
 
 public class GUIFrame extends JFrame {
 
+    private JPanel currentPanel;
+
     public GUIFrame() {
-        setContentPane(new Login(this).getJPanel());
+        currentPanel = new Login(this).getJPanel();
+        setContentPane(currentPanel);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+    }
+
+    public void changeJPanel(JPanel panel) {
+        currentPanel.removeAll();
+        getContentPane().setVisible(false);
+        getContentPane().remove(currentPanel);
+        currentPanel = panel;
+        getContentPane().add(currentPanel);
+        getContentPane().setVisible(true);
     }
 
     public static void main(String args[]) {
