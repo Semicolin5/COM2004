fdcfba9d9957e1a7cff30952a40cd221c4468e94
danny <dannycg1996@gmail.com>
2018-11-11 11:57:15 +0000

    Course Management button now works

--- src/src/gui/Welcome.java
+++ src/src/gui/Welcome.java
@@ -2,6 +2,8 @@ package src.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Welcome extends Form {
     private JPanel panel;
@@ -10,20 +12,22 @@ public class Welcome extends Form {
         super(frame);
         panel = initPanel();
         setJPanel(panel);
+
     }
 
     private JPanel initPanel() {
+        JButton courseButton = new JButton("Course Management");
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
-
         int privilege = getFrame().getUser().getPriv();
 
         //Admin buttons
         if(privilege == 4) {
             panel.add(new JButton("User Management"));
             panel.add(new JButton("Department Management"));
-            panel.add(new JButton("Course Management"));
+            panel.add(courseButton);
             panel.add(new JButton("Module Management"));
+            courseButton.addActionListener(new WelcomeHandler());
         }
         //Registrar
         else if(privilege == 3) {
@@ -38,7 +42,14 @@ public class Welcome extends Form {
         else {
             panel.add(new JButton("View Record"));
         }
-
         return panel;
     }
+
+    public class WelcomeHandler implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            changeJPanel(new Course(getFrame()).getJPanel());
+        }
+    }
+
+
 }
