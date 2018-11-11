f8abfe452d375aca9c4e7aa0b7aea3c155e19cce
danny <dannycg1996@gmail.com>
2018-11-11 11:59:28 +0000

    Course Management Home Page

---
+++ src/src/gui/Course.java
@@ -0,0 +1,33 @@
+package src.gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Course extends Form {
+    private JPanel panel;
+
+    public Course(GUIFrame frame) {
+        super(frame);
+        panel = initPanel();
+        setJPanel(panel);
+    }
+
+    private JPanel initPanel() {
+        JButton createDegree = new JButton("Create Degree");
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        panel.add(new JButton("Create Degree"));
+        panel.add(new JButton("Delete Degree"));
+        return panel;
+    }
+
+    public class CourseHandler implements ActionListener {
+        public void actionPerformed(ActionEvent actionEvent) {
+            //TODO: ADD FUNCNTIONALITY HERE
+        }
+    }
+
+
+}
