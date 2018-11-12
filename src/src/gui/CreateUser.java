4ea50cda62c3be4fdadf2a2e750a65dd33c10341
Semicolin <colin.hutchings5@gmail.com>
2018-11-12 16:09:21 +0000

    capitalised the class CreateUser.java

--- src/src/gui/CreateUser.java
+++ src/src/gui/CreateUser.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class createUser {
+public class CreateUser {
     private JPanel panel1;
     private JTextField textField1;
     private JComboBox comboBox1;
@@ -12,7 +12,7 @@ public class createUser {
     private JTextField textField3;
     private JButton createAccountButton;
 
-    public createUser() {
+    public CreateUser() {
         createAccountButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -23,7 +23,7 @@ public class createUser {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Users");
-        frame.setContentPane(new createUser().panel1);
+        frame.setContentPane(new CreateUser().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setSize(400, 200);
