9e4f0628c27ccc28d85968b96fde554ec92e3f05
danny <dannycg1996@gmail.com>
2018-11-12 02:28:44 +0000

    Better formatted CreatedDegree page (small change)

--- src/src/gui/CreateDegree.java
+++ src/src/gui/CreateDegree.java
@@ -1,39 +1,31 @@
-package gui;
+package src.gui;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CreateDegree {
+public class CreateDegree extends src.gui.Form {
     private JPanel panel1;
     private JTextField degreeCode;
     private JTextField degreeName;
     private JButton createDegree;
 
-    public CreateDegree() {
-        createDegree.addActionListener(new ActionListener() {
-            private src.db_handler.DatabaseHandler db;
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                db = new src.db_handler.DatabaseHandler();
-                src.db_handler.AdditionQueries additionQ = new src.db_handler.AdditionQueries(db); //^TODO CHECK OUT THIS ERROR
-                additionQ.addDegree(degreeCode.getText(), degreeName.getText());
-                JOptionPane.showMessageDialog(null, "Degree " +  degreeCode.getText() + " Created");
-            }
-        });
+    public class CreateDegreeHandler implements ActionListener {
+        private src.db_handler.DatabaseHandler db;
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            db = new src.db_handler.DatabaseHandler();
+            src.db_handler.AdditionQueries additionQ = new src.db_handler.AdditionQueries(db); //^TODO CHECK OUT THIS ERROR
+            additionQ.addDegree(degreeCode.getText(), degreeName.getText());
+            JOptionPane.showMessageDialog(null, "Degree " + degreeCode.getText() + " Created");
+        }
     }
 
-    public static void main(String[] args) {
-        /**
-         * Creates a GUI in Java Swing, which can be used to easily enter data
-         * which will then be saved to the SQL database.
-         * */
-        JFrame frame = new JFrame("CreateDegree");
-        frame.setContentPane(new CreateDegree().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setSize(550, 140);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    public CreateDegree(GUIFrame frame) {
+        super(frame);
+        setJPanel(panel1);
+        createDegree.addActionListener(new CreateDegreeHandler());
     }
+
 }
