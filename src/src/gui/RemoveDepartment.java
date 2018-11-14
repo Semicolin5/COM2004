caa9e656cb2244bcfe375450658b3249b0628dc4
danny <dannycg1996@gmail.com>
2018-11-14 16:02:31 +0000

    RemoveDepartment form created.

---
+++ src/src/gui/RemoveDepartment.java
@@ -0,0 +1,10 @@
+package gui;
+
+import javax.swing.*;
+
+public class RemoveDepartment {
+    private JPanel panel1;
+    private JList moduleList;
+    private JButton removeDepartmentButton;
+    private JButton homeButton;
+}
