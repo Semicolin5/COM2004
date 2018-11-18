fbb4ae7d491ae69fbf706e7eb63d5b0a0ac91aa2
Semicolin <colin.hutchings5@gmail.com>
2018-11-18 17:59:56 +0000

    function to check new user input nearly done. WIP

--- src/src/model/CheckInput.java
+++ src/src/model/CheckInput.java
@@ -9,18 +9,28 @@ public class CheckInput {
 	private static String regDegree = "[A-Z]{3}[UP][0-9]{2}";
 	private static String regModule = "[A-Z]{3}[0-9]{4}";
 	private static String regDepartment = "[A-Z]{3}";
+	private static String regLoginID = "[0-9]{4}";
+	
+	private static String regPassword = "[0-9]{1,}&&[a-z]{1,}&&[!#$%&'()*+,-./:;<=>?@^_`{|}~]{1,}";
 	
 	//Currently just a class to check regex string checking etc.
 	
 	public static void main(String[] args) {
-		System.out.println(checkDegreeCode("COMU02"));
+		/*System.out.println(checkDegreeCode("COMU02"));
 		System.out.println(checkDegreeCode("COMU032"));
 		System.out.println(checkDegreeCode("BAHP02"));
 		
 		System.out.println(checkModuleCode("COM1003"));
 		System.out.println(checkModuleCode("COM10233"));
-		System.out.println(checkModuleCode("BA1023"));
+		System.out.println(checkModuleCode("BA1023"));*/
 		
+		System.out.println(checkPassword("9lHo!"));
+		
+	}
+	
+	
+	public static boolean checkPassword (String password) {
+		return Pattern.matches(regPassword, password);
 	}
 	
 	
@@ -36,6 +46,10 @@ public class CheckInput {
 	
 	public static boolean checkDeparmentCode(String departmentCode) {
 		return Pattern.matches(regDepartment,departmentCode);
+	}
+	
+	public static boolean checkLoginID(String loginID) {
+		return Pattern.matches(regLoginID, loginID);
 	}
 	
 	//Other testing methods
@@ -54,6 +68,29 @@ public class CheckInput {
 		return input;
 	}
 	
+	
+	public static
+	
+	
+	/**
+	 * 
+	 */
+	public static String checkInputUser(String loginID, String password, String confirmPassword) {
+		String returnMessage = "";
+		//Lets do our error checks
+		if (!checkLoginID(loginID)) {
+			returnMessage = "Incorrect LoginID format.";
+		}
+		else if (/*check for uniqueness*/) {
+		}
+		else if (password != confirmPassword) {
+			returnMessage = "Passwords do not match.";
+		}
+		else {
+			returnMessage = "Accepted";
+		}
+		return returnMessage;
+	}
 	
 
 }
