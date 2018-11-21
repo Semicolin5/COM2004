a795daeb8f1e1fe16018fa004a23be4a0acb3437
Semicolin <colin.hutchings5@gmail.com>
2018-11-21 14:14:13 +0000

    More database stuff

--- SQL Files/build_database.sql
+++ SQL Files/build_database.sql
@@ -2,7 +2,7 @@
 CREATE TABLE users (
     login_id int,
     salt CHAR (32),
-    password CHAR (64), -- stores hash code
+    hashpass CHAR (64), -- stores hash code
     privilege INT NOT NULL,
     PRIMARY KEY (login_id)
 );
@@ -18,7 +18,7 @@ CREATE TABLE student (
     degree_code CHAR (6), -- the degree taken
     PRIMARY KEY (login_id),
     FOREIGN KEY (login_id) REFERENCES users (login_id),
-    FOREIGN KEY (degree_code) REFERENCES degree (code)
+    FOREIGN KEY (degree_code) REFERENCES degree (degree_code)
 );
 
 #3NF
@@ -32,56 +32,62 @@ CREATE TABLE period_of_study (
     FOREIGN KEY (login_id) REFERENCES student (login_id)
 );
 
-
-CREATE TABLE degree (
-    code CHAR (6),
-    name VARCHAR (100),
-    masters BOOLEAN,
-    PRIMARY KEY (code)
+#3NF
+CREATE TABLE department (
+    department_code CHAR (3),
+    department_name VARCHAR (100),
+    PRIMARY KEY (department_code)
 );
 
-
-
-
-
-CREATE TABLE department (
-    code CHAR (3),
-    name VARCHAR (100),
-    PRIMARY KEY (code)
+#3NF
+CREATE TABLE degree (
+    degree_code CHAR (6),
+    degree_name VARCHAR (100),
+    masters BOOLEAN,
+    year_industry BOOLEAN,
+    PRIMARY KEY (degree_code)
 );
 
 -- linker table to store how the departments associated with each degrees, 
 -- and whether a department is the lead department
-CREATE TABLE associated_department (
+-- Linker Tables
+#3NF
+CREATE TABLE degree_department (
     degree_code CHAR (6),
     department_code CHAR (3),
     lead_department BOOLEAN,
     PRIMARY KEY (degree_code, department_code),
-    FOREIGN KEY (degree_code) REFERENCES degree (code),
-    FOREIGN KEY (department_code) REFERENCES department (code)
+    FOREIGN KEY (degree_code) REFERENCES degree (degree_code),
+    FOREIGN KEY (department_code) REFERENCES department (department_code)
 );
 
+#3NF
 CREATE TABLE module (
-    code CHAR (7),
-    name VARCHAR (100),
-    credits VARCHAR (3), 
-    PRIMARY KEY (code)
+    module_code CHAR (7),
+    module_name VARCHAR (100),
+    credits VARCHAR (3),
+    semester int,
+    PRIMARY KEY (module_code)
 );
 
 -- linker table to store how the module is associated with each degree,
 -- whether it is a core module for each degree that allows the module to be taken
-CREATE TABLE core (
+
+#3NF
+CREATE TABLE module_degree (
     module_code CHAR (7),
     degree_code CHAR (6),
-    degree_level CHAR(1), --TODO: Shouldnt this be an int?
+    degree_level CHAR(1),
     core BOOLEAN,
     PRIMARY KEY (module_code, degree_code),
-    FOREIGN KEY (module_code) REFERENCES module (code),
-    FOREIGN KEY (degree_code) REFERENCES degree (code)    
+    FOREIGN KEY (module_code) REFERENCES module (module_code),
+    FOREIGN KEY (degree_code) REFERENCES degree (degree_code)    
 );
 
 -- linker table to represent the modules a student is taking, and to store
--- their grades for the module. 
+-- their grades for the module.
+
+#3NF 
 CREATE TABLE grades (
     student_id int,
     module_code CHAR (7),
@@ -89,6 +95,6 @@ CREATE TABLE grades (
     resit_percent DECIMAL(8, 5), 
     PRIMARY KEY (student_id, module_code),
     FOREIGN KEY (student_id) REFERENCES student (login_id),
-    FOREIGN KEY (module_code) REFERENCES module (code)
+    FOREIGN KEY (module_code) REFERENCES module (modue_code)
 );
 
