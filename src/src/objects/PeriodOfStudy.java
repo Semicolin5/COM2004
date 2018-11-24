70fe4647c68fbd05aee9dce15415387cb8d711a4
Edward Higgins <ewnhiggins1@sheffield.ac.uk>
2018-11-24 19:36:53 +0000

    Added period of study object

---
+++ src/src/objects/PeriodOfStudy.java
@@ -0,0 +1,49 @@
+package src.objects;
+
+import java.util.Date;
+
+public class PeriodOfStudy {
+
+    private String loginID;
+    private String label;
+    private Date startDate;
+    private Date endDate;
+    private String levelOfStudy;
+
+    /**
+     * Builds a PeriodOfStudy object, holding POS information
+     * @param loginID String containing user's code
+     * @param label String, period of study label
+     * @param startDate Date starting date of POS
+     * @param endDate Date ending date of POS
+     * @param levelOfStudy String level of study of degree
+     */
+    public PeriodOfStudy(String loginID, String label, Date startDate,
+                         Date endDate, String levelOfStudy) {
+        this.loginID = loginID;
+        this.label = label;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.levelOfStudy = levelOfStudy;
+    }
+
+    public String getLoginID() {
+        return loginID;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public String getLevelOfStudy() {
+        return levelOfStudy;
+    }
+}
