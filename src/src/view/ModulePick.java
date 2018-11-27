01e10781731f9e593494743b0241b7a0e1bda9f8
danny <dannycg1996@gmail.com>
2018-11-27 18:44:37 +0000

    ModulePick pulls through the correct grades THANK YOU ALEX

--- src/src/view/ModulePick.java
+++ src/src/view/ModulePick.java
@@ -105,11 +105,12 @@ public class ModulePick extends Form {
                                     }
                                 }
                             }
-                            for (Grade grade : Controller.getGrades()) {
-                                System.out.println("Module Pick");
-                                System.out.println(grade.getLoginID() + grade.getModuleCode() + grade.getLabel());
-                                chosenModel.addRow(new Object[]{grade.getLoginID(), grade.getModuleCode(), grade.getLabel()});
-                            }
+                           // for (Module module : Controller.getStudentModules(Integer.parseInt(studentList.getSelectedValue().toString()))) {
+                            System.out.println("Search Grades");
+                            for (Grade grade : Controller.getStudentsGradeAtPeriod(Integer.parseInt(studentList.getSelectedValue().toString()), periodOfStudyLabel)) {
+                                    chosenModel.addRow(new Object[]{grade.getModuleCode(), "FILLER1", "FILLER 2"});
+                                }
+                            //}
                         }
                     }
                     calculateCredits();
