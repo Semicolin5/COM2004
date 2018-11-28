12ab039ed978d9f248fa27372a942dad05001fd1
Edward Higgins <ewnhiggins1@sheffield.ac.uk>
2018-11-28 18:36:47 +0000

    Removed remaining back button form infrastructure

--- src/src/view/Form.java
+++ src/src/view/Form.java
@@ -8,7 +8,6 @@ public class Form extends JPanel {
 
     private GUIFrame frame;
     private JPanel panel;
-    private JButton backButton;
 
     public Form(GUIFrame frame) {
         this.frame = frame;
@@ -28,25 +27,5 @@ public class Form extends JPanel {
 
     public void changeJPanel(JPanel panel) {
         frame.changeJPanel(panel);
-    }
-
-    public void setBackButton(JButton backButton) {
-        this.backButton = backButton;
-    }
-
-    public void setBackButtonPanel(JPanel panel) {
-        backButton.addActionListener(new backButtonHandler(panel));
-    }
-
-    private class backButtonHandler implements ActionListener {
-        private JPanel panel;
-
-        public backButtonHandler(JPanel panel) {
-            this.panel = panel;
-        }
-
-        public void actionPerformed(ActionEvent actionEvent) {
-            changeJPanel(panel);
-        }
     }
 }
